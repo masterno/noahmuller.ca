268a1e809cc6728d451af90e267f442b4f4ff9bb
Your Name <you@example.com>
2025-12-21 23:54:33 -0800

    nextjs upgrade eslint upgrade, lint fix

--- components/ThemeToggle.tsx
+++ components/ThemeToggle.tsx
@@ -25,6 +25,7 @@ export function ThemeToggle() {
 
   useEffect(() => {
     const initial = getInitialTheme();
+    // eslint-disable-next-line react-hooks/set-state-in-effect
     setTheme(initial);
     applyTheme(initial);
   }, []);
